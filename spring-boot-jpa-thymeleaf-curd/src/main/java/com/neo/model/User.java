eece67df7601cd7837e7f172a17347f61327ca31
ceb <540765772@qq.com>
2020-07-06 08:53:32 +0800

    update jpa

--- spring-boot-jpa-thymeleaf-curd/src/main/java/com/neo/model/User.java
+++ spring-boot-jpa-thymeleaf-curd/src/main/java/com/neo/model/User.java
@@ -1,11 +1,9 @@
 package com.neo.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "user")
 public class User {
     @Id
     @GeneratedValue
